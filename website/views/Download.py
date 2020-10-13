f1e2cdf359dff329d9607b2fc625a8eda72849b8
Thomas Roder <troder@app23.binf.unibe.ch>
2020-10-13 11:24:06 +0200

    made nginx X-Accel-Redirect work

--- website/views/Download.py
+++ website/views/Download.py
@@ -7,36 +7,41 @@ def download_view(request):
     assert '/../' not in request.path
     assert request.path.startswith('/download/')
 
+    abs_path = os.path.join(settings.GENOMIC_DATABASE, request.path[10:])
+    filename = os.path.basename(request.path)
+
     if settings.DEBUG:
         # in debug mode, simply return the folder/file
-        path = os.path.join(settings.GENOMIC_DATABASE, request.path[10:])
-        if os.path.isdir(path):
-            return HttpResponse('\n'.join(os.listdir(path)))
+        if os.path.isdir(abs_path):
+            return HttpResponse('\n'.join(os.listdir(abs_path)))
 
-        if os.path.isfile(path):
+        elif os.path.isfile(abs_path):
             response = HttpResponse(content_type='text/plain')
-            response.content = open(path).read()
-            response["Content-Disposition"] = F"attachment; filename={os.path.basename(path)}"
+            response.content = open(abs_path).read()
+            response["Content-Disposition"] = F"attachment; filename={filename}"
+            response['Content-Type'] = 'text/plain; charset=utf-8'
             return response
 
-        return HttpResponseNotFound('<h1>Page not found</h1>')
+        else:
+            return HttpResponseNotFound('<h1>Page not found</h1>')
     else:
         # in production mode, expect nginx redirect setup: https://wellfire.co/learn/nginx-django-x-accel-redirects/
         ##############################################
-        # location /download/ {
-        #   internal;
-        #   alias   /path/to/database;
-        # }
+	    # location ^~ /protected {
+	    #     internal;
+	    #     autoindex on;
+	    #     alias /home/troder/database;
+	    # }
         ##############################################$
-        filename = os.path.basename(request.path)
-        new_path = request.path.replace('/download/', '/protected/', 1)
+        redirect_path = request.path.replace('/download/', '/protected/', 1)
         
-        response = HttpResponse()
-        del response['Content-Type']
-        del response['Accept-Ranges']
-        del response['Set-Cookie']
-        del response['Cache-Control']
-        del response['Expires']
-        response['X-Accel-Redirect'] = new_path
-        response['Content-Disposition'] = F'attachment; filename="{filename}"'
+        response = HttpResponse(200)
+        response['X-Accel-Redirect'] = redirect_path
+
+        if os.path.isfile(abs_path):
+            response['Content-Disposition'] = F'attachment; filename="{filename}"'
+            response['Content-Type'] = 'text/plain; charset=utf-8'
+        else:
+            response['Content-Type'] = 'text/html; charset=utf-8'
+
         return response
