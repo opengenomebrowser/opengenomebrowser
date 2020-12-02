d41eaedaa6e3564115e30752e68f2ec0b6232f2e
Thomas Roder <thomas.roder@bioinformatics.unibe.ch>
2020-12-02 17:30:30 +0100

    added user menu. fixes #2

--- website/static/global/js/sidebar.js
+++ website/static/global/js/sidebar.js
@@ -5,12 +5,12 @@ function openNav(event) {
 
     event.stopPropagation()
 
-    document.getElementById("mySidenav").style.width = "500px";
+    document.getElementById("ogbSidenav").style.width = "500px";
 
     // add listener to close the menu
     if (!sidebarlisterner_running) {
         $(document).on('click', function (e) {
-            if (e.altKey || e.metaKey || $('#mySidenav').has(e.target).length == 1 || $('#mySidenav').is(e.target)) {
+            if (e.altKey || e.metaKey || $('#ogbSidenav').has(e.target).length == 1 || $('#ogbSidenav').is(e.target)) {
                 // ignore clicks with alt or meta keys
                 // ignore clicks on dropdown
             } else {
@@ -25,12 +25,12 @@ function openNav(event) {
 
 /* Set the width of the side navigation to 0 */
 function closeNav(event) {
-    document.getElementById("mySidenav").style.width = "0"
+    document.getElementById("ogbSidenav").style.width = "0"
 }
 
-/* Toggle */
+/* Toggle Navbar */
 function toggleNav(event) {
-    var side_bar_state = document.getElementById("mySidenav").style.width
+    var side_bar_state = document.getElementById("ogbSidenav").style.width
 
     if (side_bar_state == "" || side_bar_state == "0px") {
         openNav(event);
