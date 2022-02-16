0c7d8eaab38d5c1e1eb6acf2cf68bb9c98046b39
MrTomRod <roder.thomas@gmail.com>
2022-02-16 15:36:57 +0100

    - fix broken link in admin/genome-import - fix error message on dna-feature-viewer.js

--- website/static/dna-feature-viewer/js/dna-feature-viewer.js
+++ website/static/dna-feature-viewer/js/dna-feature-viewer.js
@@ -6,10 +6,22 @@
 async function load_dna_feature_viewer_single(gene_identifier, target_div, span = 30000) {
     console.log('gene_identifier  :', gene_identifier, span)
 
-    await $.post('/api/dna-feature-viewer-single/', {'gene_identifier': gene_identifier, 'span': span}, function (data) {
+    await $.post('/api/dna-feature-viewer-single/', {
+        'gene_identifier': gene_identifier,
+        'span': span
+    }, function (data) {
         target_div.append(data['plot_div'])
         eval(data['script'])
     }, "json")
+        .fail(function (data, textStatus, jqXHR) {
+            if (data?.status === 409) {
+                console.log('Error 409 info:', data, textStatus, jqXHR)
+                alertModal('info', 'Info', data?.responseJSON?.message)
+            } else {
+                console.log('fail', data, textStatus, jqXHR)
+                alertModal('danger', 'Error', data?.responseJSON?.message || `(no message) ${data?.status} ${data?.statusText}`)
+            }
+        })
 }
 
 /**
@@ -26,21 +38,31 @@ async function load_dna_feature_viewer_multi(gene_identifiers, target_div, span 
     }, function (data) {
         target_div.append(data['plot_div'])
         eval(data['script'])
-    }, "json").done(function () {
-        // make loci sortable
-        new Sortable(document.getElementById('gene-loci'), {
-            handle: '.handle',
-            animation: 150
-        })
+    }, "json")
+        .done(function () {
+            // make loci sortable
+            new Sortable(document.getElementById('gene-loci'), {
+                handle: '.handle',
+                animation: 150
+            })
 
-        // enable click menu
-        $('.ogb-tag.gene').click(function (event) {
-            showGeneClickMenu(event) // , 'auto', '#gene-loci')
+            // enable click menu
+            $('.ogb-tag.gene').click(function (event) {
+                showGeneClickMenu(event) // , 'auto', '#gene-loci')
+            })
+            $('.ogb-tag.taxid').click(function (event) {
+                showTaxidClickMenu(event)
+            })
         })
-        $('.ogb-tag.taxid').click(function (event) {
-            showTaxidClickMenu(event)
+        .fail(function (data, textStatus, jqXHR) {
+            if (data?.status === 409) {
+                console.log('Error 409 info:', data, textStatus, jqXHR)
+                alertModal('info', 'Info', data?.responseJSON?.message)
+            } else {
+                console.log('fail', data, textStatus, jqXHR)
+                alertModal('danger', 'Error', data?.responseJSON?.message || `(no message) ${data?.status} ${data?.statusText}`)
+            }
         })
-    })
 }
 
 function geneLabelClicked(label) {
